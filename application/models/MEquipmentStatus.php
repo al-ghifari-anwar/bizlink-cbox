6736a6ab88142de5b2a3fe37020874dd21beebde
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2024-10-31 13:08:35 +0700

    Get equipment status

--- application/models/MEquipmentStatus.php
+++ application/models/MEquipmentStatus.php
@@ -4,16 +4,16 @@ class MEquipmentStatus extends CI_Model
 {
     public function get()
     {
-        $this->db->order_by('tb_equipment.created_at', 'DESC');
-        $result = $this->db->get('tb_equipment')->result_array();
+        $this->db->order_by('tb_equipment_status.created_at', 'DESC');
+        $result = $this->db->get('tb_equipment_status')->result_array();
 
         return $result;
     }
 
     public function getByName($name)
     {
-        $this->db->order_by('tb_equipment.created_at', 'DESC');
-        $result = $this->db->get_where('tb_equipment', ['name_equipment' => $name])->result_array();
+        $this->db->order_by('tb_equipment_status.created_at', 'DESC');
+        $result = $this->db->get_where('tb_equipment_status', ['name_equipment' => $name])->result_array();
 
         return $result;
     }
