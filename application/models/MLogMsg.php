940c43a4a692b3227e3df775ffbbfeea56bb3b9d
Al Ghifari Anwar <alghifari.anwar2002@gmail.com>
2024-11-20 01:51:31 +0700

    log msg

--- application/models/MLogMsg.php
+++ application/models/MLogMsg.php
@@ -5,7 +5,7 @@ class MLogMsg extends CI_Model
     public function get()
     {
         $this->db->order_by('date_msg', 'DESC');
-        $result = $this->db->get('tb_log_msg');
+        $result = $this->db->get('tb_log_msg')->result_array();
 
         return $result;
     }
